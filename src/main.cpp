728d85fd5dff65d46a236af2bffae8bc18bd4734
gerhead <you@example.com>
2024-08-15 22:57:27 +0500

    style changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,47 +8,20 @@
 using namespace s21;
 
 int main() {
-    GameType gameType = getGameType();
-    ViewType viewType = getViewType();
+  GameType gameType = getGameType();
+  ViewType viewType = getViewType();
 
-    GameFactory factory;
+  GameFactory factory;
 
-    IModel *model = factory.createModel(gameType);
+  IModel *model = factory.createModel(gameType);
 
-    Controller controller(model);
+  Controller controller(model);
 
-    IView *view = factory.createView(viewType, controller);
+  IView *view = factory.createView(viewType, controller);
 
-    view->startEventLoop();
+  view->startEventLoop();
 
-    delete view;
+  delete view;
 
-   return 0;
+  return 0;
 }
-
-// int main(int argc, char *argv[]) {
-//     QApplication app(argc, argv);
-
-//     Model model(nullptr);
-
-//     ViewModel viewModel(&model);
-//     View view(&viewModel);
-
-//     QMainWindow window;
-//     window.setCentralWidget(&view);
-//     window.show();
-//     view.show();
-
-//     return app.exec();
-// }
-
-// int main(int argc, char *argv[])
-// {
-//     QApplication app(argc, argv);
-
-//     Model model;
-//     View view(&model);
-//     view.show();
-
-//     return app.exec();
-// }
